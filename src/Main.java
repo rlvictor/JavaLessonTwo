6ffc3247630f77c91bf70595daaef13df21d543d
Vityad888@yandex.ru <Vityad888@yandex.ru>
2023-05-11 14:06:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15000;
+        int bonus = 20;
+        int result;
+
+        result = price / bonus;
+
+        System.out.println("Начислено " + result + " бонусов");
+    }
+}
